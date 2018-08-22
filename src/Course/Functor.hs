549ae9d7f0a1835d35e683ed5cf3d2d8b7e5329c
Sendy Halim <sendyhalim93@gmail.com>
2018-08-22 09:31:48 +0800

    Add functor solutions

--- src/Course/Functor.hs
+++ src/Course/Functor.hs
@@ -1,14 +1,14 @@
-{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE InstanceSigs        #-}
+{-# LANGUAGE NoImplicitPrelude   #-}
 {-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE InstanceSigs #-}
 
 module Course.Functor where
 
-import Course.Core
-import Course.ExactlyOne
-import Course.Optional
-import Course.List
-import qualified Prelude as P(fmap)
+import           Course.Core
+import           Course.ExactlyOne
+import           Course.List
+import           Course.Optional
+import qualified Prelude           as P (fmap)
 
 -- | All instances of the `Functor` type-class must satisfy two laws. These laws
 -- are not checked by the compiler. These laws are given as:
@@ -41,8 +41,7 @@ instance Functor ExactlyOne where
     (a -> b)
     -> ExactlyOne a
     -> ExactlyOne b
-  (<$>) =
-    error "todo: Course.Functor (<$>)#instance ExactlyOne"
+  f <$> ExactlyOne a = ExactlyOne $ f a
 
 -- | Maps a function on the List functor.
 --
@@ -56,8 +55,8 @@ instance Functor List where
     (a -> b)
     -> List a
     -> List b
-  (<$>) =
-    error "todo: Course.Functor (<$>)#instance List"
+  _ <$> Nil = Nil
+  f <$> (x :. xs) = (f x) :. (f <$> xs)
 
 -- | Maps a function on the Optional functor.
 --
@@ -71,8 +70,9 @@ instance Functor Optional where
     (a -> b)
     -> Optional a
     -> Optional b
-  (<$>) =
-    error "todo: Course.Functor (<$>)#instance Optional"
+  _ <$> Empty = Empty
+  f <$> Full a = Full $ f a
+
 
 -- | Maps a function on the reader ((->) t) functor.
 --
@@ -83,8 +83,7 @@ instance Functor ((->) t) where
     (a -> b)
     -> ((->) t a)
     -> ((->) t b)
-  (<$>) =
-    error "todo: Course.Functor (<$>)#((->) t)"
+  f <$> g = f . g
 
 -- | Anonymous map. Maps a constant value on a functor.
 --
@@ -99,8 +98,7 @@ instance Functor ((->) t) where
   a
   -> f b
   -> f a
-(<$) =
-  error "todo: Course.Functor#(<$)"
+x <$ functor = const x <$> functor
 
 -- | Anonymous map producing unit value.
 --
@@ -119,8 +117,7 @@ void ::
   Functor f =>
   f a
   -> f ()
-void =
-  error "todo: Course.Functor#void"
+void functor = () <$ functor
 
 -----------------------
 -- SUPPORT LIBRARIES --
